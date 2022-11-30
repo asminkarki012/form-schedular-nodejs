c52b8539c00d5070ec7ceabd2a081aa60202911c
asminkarki012 <37731845+asminkarki012@users.noreply.github.com>
2022-11-30 17:48:35 +0545

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -2,33 +2,33 @@ function displayTime() {
   let clock = moment().format("hh:mm:ss A");
   const getClock = document.getElementById("clock");
   getClock.innerHTML = clock;
-  let form = document.getElementById("myForm");
-  let elements = form.elements;
+//   let form = document.getElementById("myForm");
+//   let elements = form.elements;
 
-  const formMsg = document.getElementById("form-msg");
+//   const formMsg = document.getElementById("form-msg");
 
-  //getting time for validation
-  let time = moment().format("hh mm ss a");
-  // let time = "05 40 00 pm";
-  time = time.split(" ");
-  hh = parseInt(time[0]);
-  mm = parseInt(time[1]);
-  ss = parseInt(time[2]);
-  day = time[3];
-  if (hh >= 05 && hh < 06) {
-    if (mm >= 30 && mm <= 59) {
-      if ((ss >= 00 && ss <= 59) || ss === 00) {
-        if (day === "pm") {
-          formMsg.innerHTML = "You can Fill Form Now";
-        }
-      }
-    }
-  } else {
-    formMsg.innerHTML = "You can only Fill this Form Between 5.30pm and 6pm";
-    for (let i = 0; i < elements.length; i++) {
-      elements[i].readOnly = true;
-    }
-  }
+//   //getting time for validation
+//   let time = moment().format("hh mm ss a");
+//   // let time = "05 40 00 pm";
+//   time = time.split(" ");
+//   hh = parseInt(time[0]);
+//   mm = parseInt(time[1]);
+//   ss = parseInt(time[2]);
+//   day = time[3];
+//   if (hh >= 05 && hh < 06) {
+//     if (mm >= 30 && mm <= 59) {
+//       if ((ss >= 00 && ss <= 59) || ss === 00) {
+//         if (day === "pm") {
+//           formMsg.innerHTML = "You can Fill Form Now";
+//         }
+//       }
+//     }
+//   } else {
+//     formMsg.innerHTML = "You can only Fill this Form Between 5.30pm and 6pm";
+//     for (let i = 0; i < elements.length; i++) {
+//       elements[i].readOnly = true;
+//     }
+//   }
 }
 const submitBtn = document.getElementById("submit-btn");
 submitBtn.addEventListener("click", (e) => {
@@ -92,6 +92,42 @@ submitBtn.addEventListener("click", (e) => {
   //     const response = await axios.reques(options);
   //     console.log(response.data);
 });
+function timeValidation() {
+  let form = document.getElementById("myForm");
+  let elements = form.elements;
+
+  const formMsg = document.getElementById("form-msg");
+
+  //getting time for validation
+  let time = moment().format("hh mm ss a");
+
+  // let time = "07 40 00 pm";
+  time = time.split(" ");
+  hh = parseInt(time[0]);
+  mm = parseInt(time[1]);
+  ss = parseInt(time[2]);
+  day = time[3];
+  if (hh >= 05 && hh < 06) {
+    if (mm >= 30 && mm <= 59) {
+      if ((ss >= 00 && ss <= 59) || ss === 00) {
+        if (day === "pm") {
+          formMsg.innerHTML = "You can Fill Form Now";
+        }
+      }
+    }
+  } else {
+    formMsg.innerHTML = "You can only Fill this Form Between 5.30pm and 6pm";
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].readOnly = true;
+      console.log(elements[i]);
+    }
+  }
+}
+
+//time validation check running every 5 minutes
+setInterval(() => {
+  timeValidation();
+}, 300000);
 
 //running displayTime function every 1 second
 setInterval(() => {
