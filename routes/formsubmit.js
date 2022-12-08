704e18f342307562cefd3d4fc5a8b7d3be4a5554
asminkarki012 <37731845+asminkarki012@users.noreply.github.com>
2022-12-08 11:16:04 +0545

    Update formsubmit.js

--- routes/formsubmit.js
+++ routes/formsubmit.js
@@ -13,9 +13,9 @@ router.post("/", verifyToken, async (req, res) => {
 
     // console.log(req.body.content);
     if (formExists) {
-      // let time = moment().format("hh mm ss a");
+      let time = moment().format("hh mm ss a");
       // time validation
-      let time = "05 40 00 pm";
+//       let time = "05 40 00 pm";
       time = time.split(" ");
       hh = parseInt(time[0]);
       mm = parseInt(time[1]);
